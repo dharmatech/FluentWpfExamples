a34e7ea268c0f019eaf5058be411a9f536cc0259
Eduardo Cavazos <wayo.cavazos@gmail.com>
2012-10-17 22:52:32 -0500

    Initial checkin

---
+++ AnimateButtonWidth/AnimateButtonWidth/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FluentWpf;
+
+namespace AnimateButtonWidth
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Title = "Animate Property using a Storyboard";
+
+            Panel panel;
+
+            Button button;
+
+            Content =
+                panel = new StackPanel()
+                    .AddChildren(
+                        button = new Button() { Content = "Button" });
+
+            var storyboard =
+                new Storyboard()
+                    .AddChildren(
+                        new DoubleAnimation()
+                        {
+                            From = 200,
+                            To = 300,
+                            Duration = new Duration(TimeSpan.FromMilliseconds(1000))
+                        }
+                            .SetTargetObject(this, button, Button.WidthProperty));
+
+            panel.AddChildren(
+                new Button() { Content = "Start" }
+                    .AddClick((s, args) => storyboard.Begin(this)));
+        }
+    }
+}
---
+++ TaskbarItemProgress/TaskbarItemProgress/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shell;
+using FluentWpf;
+
+namespace TaskbarItemProgress
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TaskbarItemInfo = new TaskbarItemInfo();
+
+            TaskbarItemInfo.ProgressValue = 0.5;
+
+            Content = new StackPanel()
+                .AddChildren(
+                    new Button() { Content = "Normal" } 
+                        .AddClick((s, e) => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal),
+                    new Button() { Content = "Paused" } 
+                        .AddClick((s, e) => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Paused),
+                    new Button() { Content = "Error" } 
+                        .AddClick((s, e) => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Error),
+                    new Button() { Content = "Indeterminate" } 
+                        .AddClick((s, e) => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Indeterminate),
+                    new Button() { Content = "None" } 
+                        .AddClick((s, e) => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.None),
+                    new Button() { Content = "Increase" } .AddClick((s, e) => TaskbarItemInfo.ProgressValue += 0.10),
+                    new Button() { Content = "Decrease" } .AddClick((s, e) => TaskbarItemInfo.ProgressValue -= 0.10));
+        }
+    }
+}
